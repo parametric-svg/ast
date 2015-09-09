8c9c8d7adcf9083b794f2237012f7cb5d8a75521
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-09 15:31:02 +0200

    Add test

--- test.js
+++ test.js
@@ -1,11 +1,17 @@
+import ast from './module';
+
 const test = require('tape-catch');
 
-test.skip('Returns an object of the right shape', (is) => {
+test('Returns an object of the right shape', (is) => {
   is.equal(
+    ast([]).type,
+    'ParametricSvgAst',
     'with the `type` property of the right value'
   );
 
   is.equal(
+    ast([]).version,
+    1,
     'with the `version` property of the right value'
   );
 
