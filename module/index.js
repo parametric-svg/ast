7203becd7cacdb684edd8cd8a0d0eaeea66ae2a3
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-09 15:54:19 +0200

    Satisfy the spec

--- module/index.js
+++ module/index.js
@@ -1,6 +1,9 @@
-export default () => {
+const Map = require('es6-map');
+
+export default (parameters) => {
   return {
     type: 'ParametricSvgAst',
     version: 1,
+    parameters: new Map(parameters),
   };
 };
