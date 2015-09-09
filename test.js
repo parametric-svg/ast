dd2944aa7a3b9155fe6d3b3a386399f434d90a5c
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-09 15:29:17 +0200

    Stub tests

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
 const test = require('tape-catch');
+
+test.skip('Returns an object of the right shape', (is) => {
+  is.equal(
+    'with the `type` property of the right value'
+  );
+
+  is.equal(
+    'with the `version` property of the right value'
+  );
+
+  is.end();
+});
+
+test.skip('Enforces types', (is) => {
+  is.equal(
+    'fails with a helpful message if `nodes` isn’t an iterator'
+  );
+
+  is.end();
+});
